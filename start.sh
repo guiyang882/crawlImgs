e335b131316cb6b3217700e19927f6c02762440e
liuguiyangnwpu <liuguiyangnwpu@163.com>
2017-03-26 19:35:59 +0800

    add some try

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for ind in $(seq 0 1)
+for ind in $(seq 0 12)
 do
-    #scrapy crawl BaiduImg -a category=$ind > run.log 2>&1 &
+    echo $ind
+    scrapy crawl BaiduImg -a category=$ind > run.log 2>&1 &
     scrapy crawl ThreeSixZeroImg -a category=$ind > run.log 2>&1 &
 done
