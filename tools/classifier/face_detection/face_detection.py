6d5c1fde98b532e93352fe6745a4e8f361960df7
liuguiyangnwpu <liuguiyangnwpu@163.com>
2017-05-05 17:14:10 +0800

    face recong add multithread process

--- tools/classifier/face_detection/face_detection.py
+++ tools/classifier/face_detection/face_detection.py
@@ -9,6 +9,8 @@ from __future__ import print_function
 
 import os
 import face_recognition
+import multiprocessing
+import time
 
 from pymongo import MongoClient
 
@@ -19,13 +21,10 @@ DBSETTINGS = {
     "MONGODB_COLLECTION":"imagetable"
 }
 
-mc = MongoClient(DBSETTINGS["MONGODB_SERVER"], DBSETTINGS["MONGODB_PORT"])
-spiderdb = mc[DBSETTINGS["MONGODB_DB"]]
-dbtable = spiderdb[DBSETTINGS["MONGODB_COLLECTION"]]
-
-def main():
-    itemlist = list(dbtable.find({'exists_person': {'$exists': False}}))
-    print(len(itemlist))
+def deal_handle(itemlist, pid):
+    mc = MongoClient(DBSETTINGS["MONGODB_SERVER"], DBSETTINGS["MONGODB_PORT"])
+    spiderdb = mc[DBSETTINGS["MONGODB_DB"]]
+    dbtable = spiderdb[DBSETTINGS["MONGODB_COLLECTION"]]
     srcprefix = "/root/SPIDERIMAGESDB/DATASOURCE/"
     for item in itemlist:
         partpath = item["imagepath"]
@@ -39,9 +38,28 @@ def main():
                 dbtable.update({"_id": item["_id"]}, {'$set': {"exists_person": 1}})
             else:
                 dbtable.update({"_id": item["_id"]}, {'$set': {"exists_person": 0}})
-            print(item["_id"])
+            print(item["_id"], pid)
         except Exception as es:
             print(es)
+
+def main():
+    mc = MongoClient(DBSETTINGS["MONGODB_SERVER"], DBSETTINGS["MONGODB_PORT"])
+    spiderdb = mc[DBSETTINGS["MONGODB_DB"]]
+    dbtable = spiderdb[DBSETTINGS["MONGODB_COLLECTION"]]
+    itemlist = list(dbtable.find({'exists_person': {'$exists': False}}))
+    totalLen = len(itemlist)
+    nthreads = 8
+    pool = multiprocessing.Pool(processes=nthreads)
+    singlepart = totalLen / nthreads
+    start = 0
+    for ind in range(nthreads+1):
+        if start < totalLen:
+            partItem = itemlist[start:start+singlepart]
+            start += singlepart
+            pool.apply_async(deal_handle, (partItem, ind+1, ))
+    pool.close()
+    pool.join()
+
 
 if __name__ == '__main__':
     main()
