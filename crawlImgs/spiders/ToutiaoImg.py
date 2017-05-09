9670b313a00e45feb42f19ae0587a21cba3fe191
liuguiyangnwpu <liuguiyangnwpu@163.com>
2017-05-09 17:43:57 +0800

    fix coding

--- crawlImgs/spiders/ToutiaoImg.py
+++ crawlImgs/spiders/ToutiaoImg.py
@@ -22,10 +22,10 @@ def getURL(index):
     val = atnow - delta
     val = int(time.mktime(val.timetuple()))
     val = str(val)
-    a = "http://www.toutiao.com/api/pc/feed/?category=gallery_detail&utm_source=toutiao&max_behot_time="+val+"&as=A135C9A18147C24&cp=5911578C32C41E1",
-    b = "http://www.toutiao.com/api/pc/feed/?category=gallery_old_picture&utm_source=toutiao&max_behot_time="+val+"&as=A165B9E121980C4&cp=5911F8304C641E1",
-    c = "http://www.toutiao.com/api/pc/feed/?category=gallery_story&utm_source=toutiao&max_behot_time="+val+"&as=A1B599C1E1B80E7&cp=5911C8A06E776E1",
-    d = "http://www.toutiao.com/api/pc/feed/?category=gallery_photograthy&utm_source=toutiao&max_behot_time="+val+"&as=A135F931B128116&cp=591168C14166DE1"
+    a = "http://www.toutiao.com/api/pc/feed/?category=gallery_detail&utm_source=toutiao&max_behot_time="+val,
+    b = "http://www.toutiao.com/api/pc/feed/?category=gallery_old_picture&utm_source=toutiao&max_behot_time="+val,
+    c = "http://www.toutiao.com/api/pc/feed/?category=gallery_story&utm_source=toutiao&max_behot_time="+val,
+    d = "http://www.toutiao.com/api/pc/feed/?category=gallery_photograthy&utm_source=toutiao&max_behot_time="+val
     tmp = [a,b,c,d]
     return tmp
 
@@ -37,13 +37,14 @@ class ToutiaoSpider(scrapy.Spider):
         super(ToutiaoSpider, self).__init__(*args,**kwargs)
         self.totalPage = 48 * 6 * 30
         self.start_urls = [
-            "http://www.toutiao.com/api/pc/feed/?category=gallery_detail&utm_source=toutiao&max_behot_time=0&as=A135C9A18147C24&cp=5911578C32C41E1",
-            "http://www.toutiao.com/api/pc/feed/?category=gallery_old_picture&utm_source=toutiao&max_behot_time=0&as=A165B9E121980C4&cp=5911F8304C641E1",
-            "http://www.toutiao.com/api/pc/feed/?category=gallery_story&utm_source=toutiao&max_behot_time=1494314535&as=A1B599C1E1B80E7&cp=5911C8A06E776E1",
-            "http://www.toutiao.com/api/pc/feed/?category=gallery_photograthy&utm_source=toutiao&max_behot_time=0&as=A135F931B128116&cp=591168C14166DE1"
+            "http://www.toutiao.com/api/pc/feed/?category=gallery_detail&utm_source=toutiao&max_behot_time=0",
+            "http://www.toutiao.com/api/pc/feed/?category=gallery_old_picture&utm_source=toutiao&max_behot_time=0",
+            "http://www.toutiao.com/api/pc/feed/?category=gallery_story&utm_source=toutiao&max_behot_time=0",
+            "http://www.toutiao.com/api/pc/feed/?category=gallery_photograthy&utm_source=toutiao&max_behot_time=0"
         ]
         for i in range(1, self.totalPage):
             self.start_urls.extend(getURL(i))
+        print(len(self.start_urls))
 
     def parse(self, response):
         sites = json.loads(response.body_as_unicode())
